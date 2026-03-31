63b538e365fb0b07a9cdf9692e8ae62b7448c433
unknown <kabilanjvrs@gmail.com>
2026-03-31 22:08:00 +0530

    feat: setup PostgreSQL connection using GORM

--- main.go
+++ main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"wallet-transfer/internal/models"
+	"wallet-transfer/migrations"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func main() {
+
+	godotenv.Load()
+
+	dsn := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", os.Getenv("DB_HOST"), os.Getenv("DB_USERNAME"), os.Getenv("DB_NAME"), os.Getenv("DB_PASSWORD")) //Build connection string
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Warn),
+	})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+
+	db.AutoMigrate(
+		&models.Wallet{},
+		&models.Transfer{},
+		&models.LedgerEntry{},
+		&models.IdempotencyRecord{},
+	)
+
+	migrations.SeedWallets(db)
 
 	r := gin.New()
 	r.Use(gin.Logger(), gin.Recovery())
